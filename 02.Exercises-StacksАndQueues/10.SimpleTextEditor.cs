50ac829321a570b39149afa4b7c345bdb6d48602
Theodor <tiapko@abv.bg>
2017-05-25 17:00:42 +0300

    02

--- 02.Exercises-StacksАndQueues/10.SimpleTextEditor.cs
+++ 02.Exercises-StacksАndQueues/10.SimpleTextEditor.cs
@@ -1,23 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _02._10.SimpleTextEditor
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var operationsCount = int.Parse(Console.ReadLine());
-            for (int i = 0; i < operationsCount; i++)
-            {
+//namespace _02._10.SimpleTextEditor
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var operationsCount = int.Parse(Console.ReadLine());
 
-                var input = Console.ReadLine();
-            }
+//            var commandsBuffer = new Stack<string>();
+//            for (int i = 0; i < operationsCount; i++)
+//            {
+//                var input = Console.ReadLine();
 
+//                List<string> splitInput = input
+//                    .Trim()
+//                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+//                    .ToList();
 
-        }
-    }
-}
+//                int commandName = 0;
+//                string commandParam = "";
+
+//                //add          1 string 
+//                //remove last  2 count
+//                //print index  3 index
+//                //undo         4
+//                if (splitInput.Count == 2)
+//                {
+//                    commandName = int.Parse(splitInput[0]);
+//                    commandParam = splitInput[1];
+//                }
+//                if (splitInput.Count == 1)
+//                {
+//                    commandName = int.Parse(splitInput[0]);
+//                }
+
+//                switch (commandName)
+//                {
+//                    case 1:
+//                        if (commandsBuffer.Count >= 1)
+//                        {
+//                            var text = commandsBuffer.Peek();
+//                            text += commandParam;
+//                            commandsBuffer.Push(text);
+//                        }
+//                        else
+//                        {
+//                            commandsBuffer.Push(commandParam);
+//                        }
+//                        break;
+//                    case 2:
+//                        if (commandsBuffer.Count >= 1)
+//                        {
+//                            var text = commandsBuffer.Peek();
+//                            int cut = int.Parse(commandParam);
+//                            if (text.Length >= cut)
+//                            {
+//                                text = text.Substring(0,text.Length - cut);
+//                            }
+//                            else
+//                            {
+//                                text = "";
+//                            }
+//                            commandsBuffer.Push(text);
+//                        }
+//                        break;
+//                    case 3:
+//                        if (commandsBuffer.Count >= 1)
+//                        {
+//                            var text = commandsBuffer.Peek();
+//                            int index = int.Parse(commandParam);
+//                            if (index >= 1 && index <= text.Length)
+//                            {
+//                                Console.WriteLine(text[index - 1]);
+//                            }
+//                        }
+//                        break;
+//                    case 4:
+//                        if (commandsBuffer.Count >= 1)
+//                        {
+//                            commandsBuffer.Pop();
+//                        }
+//                        break;
+
+//                }
+//                //if (commandsBuffer.Count >= 1)
+//                //    Console.WriteLine(commandsBuffer.Peek());
+
+//            }
+//        }
+//    }
+//}
---
+++ 02.Exercises-StacksАndQueues/11.PoisonousPlants.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PoisonousPlants
+{
+    public static void Main()
+    {
+
+        // This is not my solution, my one gets 77/100
+        var n = int.Parse(Console.ReadLine());
+        var plants = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .Take(n)
+            .ToArray();
+
+        var days = new int[n];
+        var indexes = new Stack<int>();
+        indexes.Push(0);
+
+        for (int i = 1; i < plants.Length; i++)
+        {
+            int maxDays = 0;
+
+            while (indexes.Count > 0 && plants[indexes.Peek()] >= plants[i])
+            {
+                maxDays = Math.Max(maxDays, days[indexes.Pop()]);
+            }
+
+            if (indexes.Count > 0)
+            {
+                days[i] = maxDays + 1;
+            }
+
+            indexes.Push(i);
+        }
+
+        Console.WriteLine(days.Max());
+    }
+}
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Permissions;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _01.ReverseStrings
+//{
+//    class Plant
+//    {
+//        public Plant(double pest, bool alive)
+//        {
+//            this.Pesticide = pest;
+//            this.isAlive = alive;
+//        }
+//        public double Pesticide { get; set; }
+//        public bool isAlive { get; set; }
+
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var plantsCount = int.Parse(Console.ReadLine());
+
+//            var input = Console.ReadLine();
+
+//            List<double> plantsPest = input
+//                .Trim()
+//                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+//                .Select(double.Parse)
+//                .ToList();
+
+//            List<Plant> plants = new List<Plant>();
+//            foreach (double pest in plantsPest)
+//            {
+//                plants.Add(new Plant(pest, true));
+//            }
+
+//            int days = 0;
+
+//            if (plants.Count == 1)
+//            {
+//                Console.WriteLine(0);
+//                return;
+//            }
+
+//            while (true)
+//            {
+//                //var beforeDay = plants.Count(x => x.isAlive);
+//                bool isChanging = false;
+//                for (int i = 1; i < plants.Count; i++)
+//                {
+//                    if (plants[i].Pesticide > plants[i - 1].Pesticide)
+//                    {
+//                        plants[i].isAlive = false;
+//                        isChanging = true;
+//                    }
+//                }
+
+//                //var afterDay = plants.Count(x => x.isAlive);
+//                if (!isChanging)
+//                {
+//                    Console.WriteLine(days);
+//                    return;
+//                }
+//                else
+//                {
+//                    days++;
+//                    plants = plants.Where(x => x.isAlive).ToList();
+//                }
+
+//            }
+//        }
+//    }
+//}
