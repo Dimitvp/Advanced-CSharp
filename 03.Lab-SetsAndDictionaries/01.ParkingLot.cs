9e2d15b5ce7980020c17f7088544cadfd964fd98
Theodor <tiapko@abv.bg>
2017-05-27 13:38:00 +0300

    Lab: Sets And Dictionaries

---
+++ 03.Lab-SetsAndDictionaries/01.ParkingLot.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace _01.ReverseStrings
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string input = Console.ReadLine();
+//            var parking = new SortedSet<string>();
+
+//            while (input != "END")
+//            {
+//                var splitInput = Regex.Split(input, ", ").ToArray();
+//                var opperation = splitInput[0];
+//                var plateNumber = splitInput[1];
+//                if (opperation == "IN")
+//                {
+//                    parking.Add(plateNumber);
+//                }
+//                else
+//                {
+//                    parking.Remove(plateNumber);
+//                }
+//                input = Console.ReadLine();
+//            }
+
+
+//            if (parking.Count != 0)
+//            {
+//                foreach (var car in parking)
+//                {
+//                    Console.WriteLine(car);
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Parking Lot is Empty");
+//            }
+
+
+//        }
+//    }
+//}
---
+++ 03.Lab-SetsAndDictionaries/02.SoftUniParty.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _01.ReverseStrings
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string input = Console.ReadLine();
+//            var partiPool = new SortedSet<string>();
+
+//            while (input!= "PARTY")
+//            {
+//                partiPool.Add(input);
+//                input = Console.ReadLine();
+//            }
+
+//            while (input != "END")
+//            {
+//                partiPool.Remove(input);
+//                input = Console.ReadLine();
+//            }
+//            Console.WriteLine(partiPool.Count);
+//            foreach (var missingPerson in partiPool)
+//            {
+//                Console.WriteLine(missingPerson);
+//            }
+//        }
+//    }
+//}
