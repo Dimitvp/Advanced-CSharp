e673b6f5f325b559c6ec7eba725f895f6cdfe3c2
Theodor <tiapko@abv.bg>
2017-05-27 14:11:59 +0300

    03,04

---
+++ 03.Lab-SetsAndDictionaries/03.CountSameValuesInArray.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace _01.ReverseStrings
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            string input = Console.ReadLine();
+//            List<double> splitInput = input
+//                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+//                .Select(double.Parse)
+//                .ToList();
+
+//            var valuesCounter = new SortedDictionary<double, int>();
+
+//            foreach (var digit in splitInput)
+//            {
+//                if (!valuesCounter.ContainsKey(digit))
+//                {
+//                    valuesCounter[digit] = 0;
+//                }
+//                valuesCounter[digit]++;
+//            }
+
+//            NumberFormatInfo nfi = new NumberFormatInfo();
+//            nfi.NumberDecimalSeparator = ",";
+//            foreach (var entry in valuesCounter)
+//            {
+//                Console.WriteLine($"{entry.Key.ToString(nfi)} - {entry.Value} times");
+//            }
+//        }
+//    }
+//}
---
+++ 03.Lab-SetsAndDictionaries/04.AcademyGraduation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classSize = int.Parse(Console.ReadLine());
+            var classMarks = new SortedDictionary<string, double>();
+            for (int i = 0; i < classSize; i++)
+            {
+                string name = Console.ReadLine();
+                string gradesData = Console.ReadLine();
+                var avgGrades = gradesData
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToList()
+                    .Average();
+
+                if (!classMarks.ContainsKey(name))
+                {
+                    classMarks[name] = 0;
+                }
+                classMarks[name] += avgGrades;
+            }
+
+            foreach (var student in classMarks)
+            {
+                Console.WriteLine($"{student.Key} is graduated with {student.Value}");
+            }
+
+        }
+    }
+}
